c781020e8ffe42da70a4bac7657bfc32b0aa44cd
ArryOliveira <113479019+ArryOliveira@users.noreply.github.com>
2022-10-12 18:02:43 -0300

    Add files via upload

---
+++ PlayDoArry.java
@@ -0,0 +1,43 @@
+package aula11_10_22;
+
+public class PlayDoArry implements IPlay {
+	public void start() {
+		for (int i = 0; i < 10; i++) {
+			System.out.print("luizao");
+		}
+		
+	}
+	
+	
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void next(int quantidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void back(int quantidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String fabricante() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
